4921e70f7b0a13284a8d9d455460bd0e5c46215f
fbrnngrh <fbrnngrh@gmail.com>
2022-11-16 08:34:05 +0800

    update

---
+++ ci-sima/application/views/v_profil.php
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+=======
+<div class="col-md-4">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            
+                        </div>
+                        <div>
+                            <div class="ibox-content border-left-right">
+                                
+                            </div>
+                        </div>
+                    </div>
+</div>
+>>>>>>> eec27884f787cdd902fa303a6880a5bbcd56b1c0
