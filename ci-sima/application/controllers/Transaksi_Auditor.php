7faaa4c1ff12eea4db8aaedd810290615f5b711e
fbrnngrh <fbrnngrh@gmail.com>
2022-11-21 16:36:11 +0800

    tes

--- ci-sima/application/controllers/Transaksi_Auditor.php
+++ ci-sima/application/controllers/Transaksi_Auditor.php
@@ -1865,11 +1865,7 @@ public function previewpart($page)
     $idjadwal_audit = $this->input->post('idjadwal_audit');
     $status = $this->input->post('status');
 
-<<<<<<< HEAD
     $count = $this->mtransauditor->countpart($cabang, $idjadwal_audit, $status);
-=======
-    $count = $this->mtransauditor->countpart1($cabang, $idjadwal_audit, $status);
->>>>>>> 7843f9e5db09bea38e3ff64a8109735db5fab085
     $this->load->library('pagination');
 
     $config['base_url'] = base_url() . 'transaksi_auditor/previewpart';
