f69d3b0bd3ed54e73c75452e44b0f6a0b1bb6657
hlmnyhya <hlmnyhya99@gmail.com>
2022-11-27 19:34:19 +0800

    rev

--- ci-api/application/models/master/m_lokasi_cabang.php
+++ ci-api/application/models/master/m_lokasi_cabang.php
@@ -24,9 +24,18 @@ class M_Lokasi_Cabang extends CI_Model {
 
     public function getLokasiRak($id = null)
     {
+        // $id = $this->db->get('gudang')->result();
+        $data = array(
+            'id_lokasi' => $id
+        );
+        $this->db->where($data);
+        $result = $this->db->get_where('lokasi_rak_bin', 'id_lokasi', $data)->result();
+        // $result = $this->db->query("SELECT * FROM lokasi_rak_bin WHERE id_lokasi = '"[$data],"'")->row_array();
+        return $result;
         
-        $result = $this->db->query("SELECT * FROM lokasi_rak_bin WHERE id_lokasi = '2NG-2NGP';")->result();
-        return $result; 
+        // $result = $this->db->query("SELECT * FROM lokasi_rak_bin WHERE id_lokasi = '".$data."'")->result();
+
+        // return $result;
         
         
         // $result = $this->db->query("select * from lokasi_rak_bin where id_lokasi = '".$id."'")->result();
