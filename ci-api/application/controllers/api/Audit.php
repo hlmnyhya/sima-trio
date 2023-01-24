b85d59d01f135f5b8cead9f58d11da5f835599a3
masSyahril <leny.setiwan@gmail.com>
2023-01-24 14:57:22 +0800

    BE REPORT

--- ci-api/application/controllers/api/Audit.php
+++ ci-api/application/controllers/api/Audit.php
@@ -1666,6 +1666,133 @@ function __construct() {
             
         }
     }
+
+    public function cetakPart_get()
+    {
+        $cabang= $this->get('id_cabang');
+        $idjadwal_audit= $this->get('idjadwal_audit');
+        $status = $this->get('status');
+        
+            $cetak= $this->mlapdat->cetakPart($cabang, $idjadwal_audit,$status);
+        if ($cetak) {
+            $this->response([
+                'status' => true,
+                'data' => $cetak
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'Data not found.'
+            ], REST_Controller::HTTP_OK);
+            
+        }
+    }
+
+    public function cetakPartSesuai_get()
+    {
+        $cabang= $this->get('id_cabang');
+        $idjadwal_audit= $this->get('idjadwal_audit');
+        $keterangan = $this->get('keterangan');
+        
+            $cetak= $this->mlapdat->cetakPartSesuai($cabang, $idjadwal_audit,$keterangan);
+        if ($cetak) {
+            $this->response([
+                'status' => true,
+                'data' => $cetak
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'Data not found.'
+            ], REST_Controller::HTTP_OK);
+            
+        }
+    }
+
+    public function cetakPartbelumditemukan_get()
+    {
+        $cabang= $this->get('id_cabang');
+        $idjadwal_audit= $this->get('idjadwal_audit');
+        $status = $this->get('status');
+        
+            $cetak= $this->mlapdat->cetakPartbelum_ditemukan($cabang, $idjadwal_audit,$status);
+        if ($cetak) {
+            $this->response([
+                'status' => true,
+                'data' => $cetak
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'Data not found.'
+            ], REST_Controller::HTTP_OK);
+            
+        }
+    }
+
+    public function cetakPartkurang_get()
+    {
+        $cabang= $this->get('id_cabang');
+        $idjadwal_audit= $this->get('idjadwal_audit');
+        $keterangan = $this->get('keterangan');
+        
+            $cetak= $this->mlapdat->cetakPartkurang($cabang, $idjadwal_audit,$keterangan);
+        if ($cetak) {
+            $this->response([
+                'status' => true,
+                'data' => $cetak
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'Data not found.'
+            ], REST_Controller::HTTP_OK);
+            
+        }
+    }
+
+    public function cetakPartlebih_get()
+    {
+        $cabang= $this->get('id_cabang');
+        $idjadwal_audit= $this->get('idjadwal_audit');
+        $keterangan = $this->get('keterangan');
+        
+            $cetak= $this->mlapdat->cetakPartkurang($cabang, $idjadwal_audit,$keterangan);
+        if ($cetak) {
+            $this->response([
+                'status' => true,
+                'data' => $cetak
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'Data not found.'
+            ], REST_Controller::HTTP_OK);
+            
+        }
+    }
+
+    public function cetakPartket_get()
+    {
+        $cabang= $this->get('id_cabang');
+        $idjadwal_audit= $this->get('idjadwal_audit');
+        $keterangan = $this->get('keterangan');
+        
+            $cetak= $this->mlapdat->cetakPartket($cabang, $idjadwal_audit,$keterangan);
+        if ($cetak) {
+            $this->response([
+                'status' => true,
+                'data' => $cetak
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'Data not found.'
+            ], REST_Controller::HTTP_OK);
+            
+        }
+    }
+    
     public function cetakUnitNotready_get()
     {
         $cabang= $this->get('id_cabang');
